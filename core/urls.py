593d66b80ab9d9809e5223daae52a71e33ab3bb3
ahmettoktasdot <ahmet.toktas@useinsider.com>
2023-01-11 17:55:48 +0300

    commit test

--- core/urls.py
+++ core/urls.py
@@ -15,7 +15,15 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.core.urls.static import static
+from django.core.urls import settings
+from blog.views import index
+import os 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', index, name = 'index')
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
